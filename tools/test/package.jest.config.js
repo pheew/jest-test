02d3454db13eec3e15eb87e59ee1c1b61ce4bb50
Niek Bruins <niek@fambruins.nl>
2020-08-21 13:56:15 +0200

    include both libs in package config

--- tools/test/package.jest.config.js
+++ tools/test/package.jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   projects: [
     {
+      displayName: "testing-library",
+      testMatch: ["**/?(*.)+(test).[jt]s?(x)"],
+    },
+    {
       displayName: "enzyme",
       testMatch: ["**/?(*.)+(spec).[jt]s?(x)"],
     },
